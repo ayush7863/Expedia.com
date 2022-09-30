b78f67c4b04e4a28873eb17013b7838a0623559e
dkpradhan99 <primewebmail3@gmail.com>
2022-09-30 14:54:09 +0530

    file added

--- script/flight.js
+++ script/flight.js
@@ -1,6 +1,4 @@
-
-import navbar from "../components/navbar.js"
-let nav = document.getElementById("navbar");
-nav.innerHTML = navbar()
-
+data=[{time:"18:15-19:55",journey:"Delhi to Patna",icon:"https://images.trvl-media.com/media/content/expus/graphics/static_content/fusion/v0.1b/images/airlines/vector/s/UK_sq.svg",
+        name:"Vistara",duration:"1h 40m (Direct)",price:"₹5,116"},{time:"18:15-19:55",journey:"Delhi to Patna",icon:"https://images.trvl-media.com/media/content/expus/graphics/static_content/fusion/v0.1b/images/airlines/vector/s/UK_sq.svg",
+        name:"Vistara",duration:"1h 40m (Direct)",price:"₹5,116"}]
 
